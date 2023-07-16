f5821be46c15c0fbafeb8ab76bfa3361a88289df
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-07-16 15:43:58 +0300

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <link rel="manifest" href='/manifest.json'></link>
-        <link rel='apple-touch-icon' href='/icon.png'></link>
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-title" content="prototurk.com" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+      <link href="/pwa/img/iphone5_splash.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+      <link href="/pwa/img/iphone6_splash.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+      <link href="/pwa/img/iphoneplus_splash.png" media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+      <link href="/pwa/img/iphonex_splash.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+      <link href="/pwa/img/ipad_splash.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+      <link href="/pwa/img/ipadpro1_splash.png" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+      <link href="/pwa/img/ipadpro2_splash.png" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+      <link rel="apple-touch-icon" sizes="128x128" href="/pwa/img/128x128.png" />
+      <link rel="apple-touch-icon-precomposed" sizes="128x128" href="/pwa/img/128x128.png" />
+      <link rel="icon" sizes="192x192" href="/pwa/img/192x192.png" />
+      <link rel="icon" sizes="128x128" href="/pwa/img/128x128.png" />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
