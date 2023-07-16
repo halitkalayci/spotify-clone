33dfd1c25c3d0661ad8c0a654218ec3257d3d6e1
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-07-16 15:45:17 +0300

    x

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,7 +3,7 @@ import SideBar from "../components/sidebar/Sidebar";
 export default function Home() {
   return (
     <SideBar>
-    <h1 className="text-green-500">Hello (Site)</h1>
+    <h1 className="text-green-500">Hello (Site) deneme</h1>
     </SideBar>
   )
 }
